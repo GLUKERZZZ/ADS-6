ea2325a9a8c248ca07fe2dbd6625b7286862759f
GLUKERZZZ <artem2801200635@gmail.com>
2025-05-04 20:32:09 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,14 +2,52 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
-
 struct SYM {
   char ch;
   int prior;
 };
 
+struct Node {
+  SYM data;
+  Node* next;
+  Node(SYM sym) : data(sym), next(nullptr) {}
+};
+class TPQueue {
+private:
+  Node* head;
+public:
+  TPQueue() : head(nullptr) {}
+  ~TPQueue() {
+    while (!isEmpty()) {
+      pop();
+    }
+  }
+  void push(SYM sym) {
+    Node* newNode = new Node(sym);
+    if (head == nullptr || sym.prior > head->data.prior) {
+      newNode->next = head;
+      head = newNode;
+    } else {
+      Node* current = head;
+      while (current->next != nullptr && sym.prior <= current->next->data.prior) {
+        current = current->next;
+      }
+      newNode->next = current->next;
+      current->next = newNode;
+    }
+  }
+  SYM pop() {
+    if (isEmpty()) {
+      return { '\0', 0 };
+    }
+    Node* temp = head;
+    SYM data = head->data;
+    head = head->next;
+    delete temp;
+    return data;
+  }
+  bool isEmpty() const {
+    return head == nullptr;
+  }
+};
 #endif  // INCLUDE_TPQUEUE_H_
